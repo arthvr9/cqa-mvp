fbd810a90e5199b8b0a1baa67b2fd969d6724a4e
arthur <heartdaplug@gmail.com>
2024-11-11 14:08:24 -0300

    Fixed

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ modelFile = "res10_300x300_ssd_iter_140000_fp16.caffemodel"
 configFile = "deploy.prototxt"
 
 net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
-
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -13,28 +12,23 @@ while True:
         break
 
     h, w = frame.shape[:2]
-    
     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
     net.setInput(blob)
-    
     detections = net.forward()
 
     for i in range(detections.shape[2]):
         confidence = detections[0, 0, i, 2]
         
         if confidence > 0.5:
-
             box = detections[0, 0, i, 3:7] * [w, h, w, h]
             (startX, startY, endX, endY) = box.astype("int")
-
             cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 255, 255), 2)
-
             label = "person"
-            cv2.putText(frame, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
+            cv2.putText(frame, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
 
-    cv2.imshow("Detecção Facial", frame)
+    cv2.imshow("cqa", frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(30) & 0xFF == ord('q'):
         break
 
 cap.release()
